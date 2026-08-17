ca1c2070ab626c5b8b946c33637a3a0e76bbde91
DIEGO RODRIGUES JOAQUIM <diego.rjoaquim@senacsp.edu.br>
2026-08-17 12:14:22 -0300

    BI

---
+++ BI/dados.py
@@ -0,0 +1,69 @@
+nome = "diego"
+idade = 34
+altura = 1.71
+verdade = False
+
+print(nome)
+print(idade)
+print(altura)
+print(verdade)
+
+produtos = [
+    {
+        "produto":"Notebook Pro 14",
+        "categoria":"Informática",
+        "quantidade":8,
+        "preco":3500
+    },
+    {
+        "produto":"Mouse Gamer",
+        "categoria":"Periférico",
+        "quantidade":35,
+        "preco":120 
+    },
+    {
+        "produto":"Teclado Mecânico",
+        "categoria":"Periférico",
+        "quantidade":18,
+        "preco":280 
+    },
+    {
+        "produto":"Monitor 24",
+        "categoria":"Informática",
+        "quantidade":12,
+        "preco":900 
+    } 
+]
+print(produtos[3]['produto'])
+print(produtos[3]['categoria'])
+
+
+for produto in produtos:
+    # print(produto)
+    
+    faturamento = (
+        produto['quantidade'] * produto['preco']
+    )
+
+    print(
+       "Produto: ", produto['produto'], "Valor unitario: ", produto['preco'], "Faturamento:  R$", faturamento
+    )
+
+
+# maior faturamento
+maior_faturamento = 0
+produto_maior_faturamento = ""
+
+for produto in produtos:
+    # print(produto)
+    
+    faturamento = (
+        produto['quantidade'] * produto['preco']
+    )
+
+    if faturamento > maior_faturamento:
+        maior_faturamento = faturamento
+        produto_maior_faturamento = (produto["produto"])
+
+print('Produto com maior faturamento: ', produto_maior_faturamento)
+print('Faturamento: R$ ', maior_faturamento)
